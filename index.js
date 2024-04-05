e57a273e01c116d4ebcc78adb2469dc4eea734fa
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-06 04:03:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function searchWord(str) {
+    return str.includes("abc");
+}
+console.log(searchWord("Here we are searching word java in this line to check"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function searchWord(str:string): boolean{
+    return str.includes("abc")
+}
+
+console.log(searchWord("Here we are searching word java in this line to check"))
